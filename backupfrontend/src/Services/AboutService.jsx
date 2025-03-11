fde5bb52e8fc6161f965757895ae1e522643fdf9
pwn671 <pwnsharma671@gmail.com>
2025-03-11 12:38:15 +0530

    Update AboutService.jsx

--- backupfrontend/src/Services/AboutService.jsx
+++ backupfrontend/src/Services/AboutService.jsx
@@ -58,7 +58,7 @@ apiClient.interceptors.response.use(
 
 export const getAbout = async () => {
   try {
-    const response = await apiClient.get('${API_URL}');
+    const response = await apiClient.get("/");
     return response.data;
   } catch (error) {
     throw error;
