3a69ff0d9ea48098a6f6ce4c2481973b205c8351
pwn671 <pwnsharma671@gmail.com>
2025-03-11 17:50:37 +0530

    Update About.js

--- Backend/routes/About.js
+++ Backend/routes/About.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 // Multer for Image Upload
 const storage = multer.diskStorage({
-  destination: "./uploads/",
+  destination: "./images/",
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
   },
